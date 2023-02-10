2b55b79ee60848a8aba59967a0f875e89b054b6f
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-02-10 17:26:31 +0100

    set up about us file with, code and file update.

---
+++ src/Pages/AboutUs.js
@@ -0,0 +1,25 @@
+import React from "react";
+ 
+export const AboutUs = () => {
+  return (
+    <div className="home">
+      <h1>The Poetic Torpedo About us</h1>
+    </div>
+  );
+};
+ 
+export const OurAim = () => {
+  return (
+    <div className="home">
+      <h1>The Poetic Torpedo Aim</h1>
+    </div>
+  );
+};
+ 
+export const OurVision = () => {
+  return (
+    <div className="home">
+      <h1>The Poetic Torpedo Vision</h1>
+    </div>
+  );
+};

51ad57e13b19e5eca159c20f79f5e416d689d7fd
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-02-10 17:27:45 +0100

    configure contact menu link.

---
+++ src/Pages/ContactUs.js
@@ -0,0 +1,11 @@
+import React from "react";
+ 
+const Contact = () => {
+  return (
+    <div className="contact">
+      <h1>The Poetic Torpedo Contact us</h1>
+    </div>
+  );
+};
+ 
+export default Contact;

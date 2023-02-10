6c07311c3ce85faec3a1a459f5afebbfdfef9a37
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-02-10 17:28:43 +0100

    implement event listener file.

---
+++ src/Pages/Events.js
@@ -0,0 +1,25 @@
+import React from "react";
+ 
+export const Events = () => {
+  return (
+    <div className="events">
+      <h1>The Poetic Torpedo Events</h1>
+    </div>
+  );
+};
+ 
+export const EventsOne = () => {
+  return (
+    <div className="events">
+      <h1>The Poetic Torpedo Event1</h1>
+    </div>
+  );
+};
+ 
+export const EventsTwo = () => {
+  return (
+    <div className="events">
+      <h1>The Poetic Torpedo Event2</h1>
+    </div>
+  );
+};

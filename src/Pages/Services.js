adf34f2b3514cc5fa85be0687801dff51c7f98f3
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-02-11 13:39:28 +0100

    code fix.

---
+++ src/Pages/Services.js
@@ -0,0 +1,33 @@
+import React from "react";
+ 
+export const Services = () => {
+  return (
+    <div className="services">
+      <h1>The Poetic Torpedo  Services</h1>
+    </div>
+  );
+};
+ 
+export const ServicesOne = () => {
+  return (
+    <div className="services">
+      <h1>The Poetic Torpedo  Service1</h1>
+    </div>
+  );
+};
+ 
+export const ServicesTwo = () => {
+  return (
+    <div className="services">
+      <h1>The Poetic Torpedo  Service2</h1>
+    </div>
+  );
+};
+ 
+export const ServicesThree = () => {
+  return (
+    <div className="services">
+      <h1>The Poetic Torpedo  Service3</h1>
+    </div>
+  );
+};

31d0b410e920ce908c1ab04afaa306742bf15c84
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2023-02-11 13:39:50 +0100

    code changes fix.

---
+++ src/Pages/Support.js
@@ -0,0 +1,11 @@
+import React from "react";
+ 
+const Support = () => {
+  return (
+    <div className="support">
+      <h1>The Poetic Torpedo  Support us</h1>
+    </div>
+  );
+};
+ 
+export default Support;
